0aca02ca4da210f86536a3adf166c2f16d44f53e
mortezabarghamadi <mortezabarghamadi5@gmail.com>
2023-02-13 00:00:07 +0330

    class jadid ezafe shod

---
+++ src/test/java/Cartest.java
@@ -0,0 +1,29 @@
+public class Cartest {
+boolean ayaMashinRoshanAst;
+boolean ayadarmashinbazast;
+
+void BazKardanDar(){
+    System.out.println("dar baz ast");
+    ayadarmashinbazast = true;
+}
+    void bastanDar(){
+        System.out.println("dar baste ast");
+        ayadarmashinbazast = false;
+}
+void roshanKardanmashin(){
+        System.out.println("mashin roshan ast");
+    ayaMashinRoshanAst = true;
+}
+    void khamoshKardanmashin(){
+        System.out.println("mashin khamosh ast");
+        ayaMashinRoshanAst = false;
+    }
+    void harekat(){
+    if( !ayaMashinRoshanAst && ayadarmashinbazast ){
+        System.out.println("mashin dar hal harekat ast");
+    }else {
+        System.out.println("mashin dar ha harekat nist");
+    }
+
+    }
+}
