1fddcfc09c7b2223f9a1dedd2f3b7a887f2325d7
mortezabarghamadi <mortezabarghamadi5@gmail.com>
2023-02-13 16:12:55 +0330

    class mashin test

--- src/test/java/Cartest.java
+++ src/test/java/Cartest.java
@@ -1,29 +1,39 @@
+import org.junit.jupiter.api.Test;
+
 public class Cartest {
-boolean ayaMashinRoshanAst;
-boolean ayadarmashinbazast;
 
-void BazKardanDar(){
-    System.out.println("dar baz ast");
-    ayadarmashinbazast = true;
-}
-    void bastanDar(){
-        System.out.println("dar baste ast");
-        ayadarmashinbazast = false;
-}
-void roshanKardanmashin(){
-        System.out.println("mashin roshan ast");
-    ayaMashinRoshanAst = true;
-}
-    void khamoshKardanmashin(){
-        System.out.println("mashin khamosh ast");
-        ayaMashinRoshanAst = false;
-    }
-    void harekat(){
-    if( !ayaMashinRoshanAst && ayadarmashinbazast ){
-        System.out.println("mashin dar hal harekat ast");
-    }else {
-        System.out.println("mashin dar ha harekat nist");
-    }
+            class carrrrtest {
 
+            boolean ayaMashinRoshanAst;
+            boolean ayadarmashinbazast;
+
+            void BazKardanDar() {
+                System.out.println("dar baz ast");
+                ayadarmashinbazast = true;
+            }
+
+            void bastanDar() {
+                System.out.println("dar baste ast");
+                ayadarmashinbazast = false;
+            }
+
+            void roshanKardanmashin() {
+                System.out.println("mashin roshan ast");
+                ayaMashinRoshanAst = true;
+            }
+
+            void khamoshKardanmashin() {
+                System.out.println("mashin khamosh ast");
+                ayaMashinRoshanAst = false;
+            }
+
+            void harekat() {
+                if (!ayaMashinRoshanAst && ayadarmashinbazast) {
+                    System.out.println("mashin dar hal harekat ast");
+                } else {
+                    System.out.println("mashin dar ha harekat nist");
+                }
+
+            }
+        }
     }
-}
