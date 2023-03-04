eee4dcc4bba1c64aafcfc668fe8be3c7ccba2664
mortezabarghamadi <mortezabarghamadi5@gmail.com>
2023-03-04 17:46:14 +0330

    taghirat ijad shod

--- src/main/java/CalculeterUI.java
+++ src/main/java/CalculeterUI.java
@@ -1,48 +1,50 @@
 import javax.swing.*;
 
 public class CalculeterUI {
-
+    private static JTextField result;
     public static void main(String[] args) {
         JFrame mashinhesab = new JFrame("mashin hesab");
-        JTextField result = new JTextField();
-        result.setBounds(0,0,200,50);
-        result.setEnabled(false);
+        result = new JTextField();
+        result.setBounds(0,0,150,50);
+        result.setEditable(false);
 
-        JButton one = new JButton("1");
-        one.setBounds(0,50,50,50);
-        JButton two = new JButton("2");
-        two.setBounds(50,50,50,50);
-        JButton three = new JButton("3");
-        three.setBounds(100,50,50,50);
+        JButton cancel = new JButton("C");
+        cancel.setBounds(150,0,50,50);
+        cancel.addActionListener(event-> result.setText(""));
+
+        JButton one = getButton("1", 0, 50);
+        JButton two = getButton("2", 50, 50);
+        JButton three = getButton("3", 100, 50);
+
         JButton plus = new JButton("+");
         plus.setBounds(150,50,50,50);
-        JButton four = new JButton("4");
-        four.setBounds(0,100,50,50);
-        JButton five = new JButton("5");
-        five.setBounds(50,100,50,50);
-        JButton six = new JButton("6");
-        six.setBounds(100,100,50,50);
+
+        JButton four = getButton("4", 0, 100);
+        JButton five = getButton("5", 50, 100);
+        JButton six = getButton("6", 100, 100);
+
         JButton minus = new JButton("-");
         minus.setBounds(150,100,50,50);
-        JButton seven = new JButton("7");
-        seven.setBounds(0,150,50,50);
-        JButton eight = new JButton("8");
-        eight.setBounds(50,150,50,50);
-        JButton nine = new JButton("9");
-        nine.setBounds(100,150,50,50);
+
+        JButton seven = getButton("7", 0, 150);
+        JButton eight = getButton("8", 50, 150);
+        JButton nine = getButton("9", 100, 150);
+
         JButton multiply = new JButton("*");
         multiply.setBounds(150,150,50,50);
-        JButton point = new JButton(".");
-        point.setBounds(0,200,50,50);
-        JButton zero = new JButton("0");
-        zero.setBounds(50,200,50,50);
+
+        JButton point = getButton(".", 0, 200);
+        JButton zero = getButton("0", 50, 200);
+
         JButton equal = new JButton("=");
         equal.setBounds(100,200,50,50);
+
         JButton division = new JButton("/");
         division.setBounds(150,200,50,50);
 
 
         mashinhesab.add(result);
+        mashinhesab.add(cancel);
         mashinhesab.add(one);
         mashinhesab.add(two);
         mashinhesab.add(three);
@@ -62,5 +64,12 @@ public class CalculeterUI {
         mashinhesab.setSize(600,600);
         mashinhesab.setLayout(null);
         mashinhesab.setVisible(true);
+    }
+
+    private static JButton getButton(String label, int xposition, int yposition) {
+        JButton button= new JButton(label);
+        button.setBounds(xposition, yposition,50,50);
+        button.addActionListener(event -> result.setText(result.getText() + button.getText()));
+        return button;
     }
 }
